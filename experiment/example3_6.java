43df5df2fef4b8ec9c64f3b7c38f0130310dddbe
小余 <qwert-f@qq.com>
2020-09-22 21:15:36 +0800

    Greek alphabet experiment

---
+++ experiment/example3_6.java
@@ -0,0 +1,14 @@
+package experiment;
+
+// output Greek alphabet
+public class example3_6 {
+    public static void main(String[] args) {
+        for (int i = 913; i <= 937; i ++){
+			System.out.print((char)i+" ");
+		}
+		System.out.println();
+		for (int i = 945; i <= 969; i ++){
+			System.out.print((char)i+" ");
+		}
+    }
+}
--- experiment/example3_7.java
+++ experiment/example3_7.java
@@ -3,6 +3,21 @@ package experiment;
 // summary the prime number within 100
 public class example3_7 {
     public static void main(String[] args) {
-        
+        int i,sum = 0,count = 0; //initial copy is 1
+        for(int j=2;j<=900;j++){
+            for(i=2;i<=j;i++){
+                if(j%i==0)
+                    break;   
+            }
+            if(i>j/2){
+                System.out.print(j+" ");
+                count = count+1;
+                sum = sum+j;
+                if(count%25==0){
+                    System.out.print("\n"); //beauty the output 
+                }
+            }
+        }
+    System.out.print("\n"+"sum is:"+sum+"\n"+count);
     }
 }
