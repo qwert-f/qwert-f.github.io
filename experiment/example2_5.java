530993b9d90258be22adfb6c9285d2fd7a636842
小余 <qwert-f@qq.com>
2020-09-29 17:18:03 +0800

    comment example2_5.java in English

--- experiment/example2_5.java
+++ experiment/example2_5.java
@@ -5,9 +5,10 @@ import java.util.*;
 class example2_5 {
     public static void main(String args[]) {
         int start = 0,end,middle;
+        // array initialization. format:int array[] = new int[n] and then init array[i] i from 0 to n.
         int a[] = {12,45,67,89,123,-45,67};
         int N = a.length;
-        for(int i=0;i < N;i++) {    //进行排序
+        for(int i=0;i < N;i++) {    //start sorting
             for(int j=i+1;j <N;j++) {
                 if(a[j] < a[i]) {
                     int t = a[j];
@@ -17,27 +18,27 @@ class example2_5 {
             }
         }
         Scanner scanner = new Scanner(System.in);
-        System.out.println("输入整数，程序判断该整数是否在数组中:");
-        int number = scanner.nextInt(); //读取输入的整数
-        int count = 0;  //给定一个记数点
+        System.out.println("input a integer,program judge if the input in array:");
+        int number = scanner.nextInt(); //read the number you've input
+        int count = 0;  //give a counter
         end = N;
-        middle = (start+end) / 2;   //这没啥好注释的
-        while(number!= a[middle]) { //与排序后的数组中间的值进行比较
+        middle = (start+end) / 2;   //nothing need to comment
+        while(number!= a[middle]) { //compare whit the midian in the array which have been sorting 
             if(number>a[middle])
-                start = middle; //输入的大于中位数，将中位数作为新的（折半后的数组）比较的起始位置
+                start = middle; //the input bigger than middle,use midian as the compare initial location of the array halved
             else if(number<=a[middle])
-                end = middle;   //与前面的相反
-            middle = (start+end) / 2;   //确定性的中位数
-            count++;    //记数点自加
+                end = middle;   //opposite with the comment before
+            middle = (start+end) / 2;   //determine the midian
+            count++;    //counter plus itself
             if(count>N/2)
-                break;  //数组折半折没了，折不了了
+                break;  //array null,can't half
         }
         if(count>N/2) 
-            //折完了还没匹配成功  
-            System.out.printf("%d不在数组中.",number); 
+            //match unsuccess after half ending  
+            System.out.printf("%d not in array.",number); 
         else 
-            //没折完就找到了
-            System.out.printf("%d在数组中.\n",number);
+            //find before half ending
+            System.out.printf("%d in array.\n",number);
             scanner.close();
     }
 }
